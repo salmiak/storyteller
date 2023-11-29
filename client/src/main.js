ef3c6fa4afb263c36049358a132c9aae14e06447
Alfred Beckman <alfred@salmiakmedia.se>
2023-11-29 22:20:20 +0100

    Remove store

--- client/src/main.js
+++ client/src/main.js
@@ -4,18 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { createPinia } from 'pinia'
-import { socketStore } from './stores/socketStore'
-
-
 const app = createApp(App)
-const pinia = createPinia()
 
-app.use(pinia)
 app.use(router)
 
 app.mount('#app')
-
-pinia.use(socketStore)
 
 
